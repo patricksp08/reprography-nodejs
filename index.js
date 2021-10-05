dc5a9f7bec4bf78baba11f255e99304952d5c1de
Lucca <mario-lucca@hotmail.com>
2021-10-05 17:08:14 -0300

    Adicionado force:true no Index.js para ele forçar a criação das tabelas no banco de dados, mesmo que elas já existam. Await Registros.Inserir serve para inserir os registros nas tabelas que contém as informações estáticas

--- index.js
+++ index.js
@@ -43,9 +43,9 @@ require('./routes/resettoken-routes')(app)
 //Swagger Routes
 require('./routes/swagger')(app)
 
-db.sequelize.sync().then(() => {
+db.sequelize.sync({force: true}).then(() => {
   app.listen(3002, async () => {
-    // await registros.Inserir();
+    await registros.Inserir();
     console.log("(||||||||| | | -------- Server running on port 3002 -------- | | |||||||||)");
   });
 });
