689398ac9e3ac413a3350e897bf53491a68db68a
Oseias-maker <oseiasfariasjesus66@gmail.com>
2021-10-27 11:09:25 -0300

    Atts

---
+++ routes/swagger-routes.js
@@ -0,0 +1,14 @@
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocsV1 = require("../.config/swagger/v1/swaggerV1.config.json");
+const swaggerDocsV2 = require("../.config/swagger/v2/swaggerV2.config.json");
+
+module.exports = function (app) {
+  //Swagger Routes
+
+  // //Versão 1
+  app.use('/v1/docs', swaggerUi.serve, (...args) => swaggerUi.setup(swaggerDocsV1)(...args));
+ 
+  //Versão 2 (falta comentar alguns campos, como na versão 1)
+  app.use('/v2/docs', swaggerUi.serve, (...args) => swaggerUi.setup(swaggerDocsV2)(...args));
+
+};
