11d442e50cf8c8a3665466a2a9b20f0822988813
Lucca <lucca@pop-os.localdomain>
2021-10-28 16:49:52 -0300

    Att!

--- src/routes/swagger-routes.js
+++ src/routes/swagger-routes.js
@@ -1,11 +1,6 @@
 const swaggerUi = require("swagger-ui-express");
-<<<<<<< HEAD
 const swaggerDocsV1 = require("../../info_api/docs/swagger/v1/swaggerV1.config.json");
 const swaggerDocsV2 = require("../../info_api/docs/swagger/v2/swaggerV2.config.json");
-=======
-const swaggerDocsV1 = require("../../info_api/documentacao/swagger/v1/swaggerV1.config.json");
-const swaggerDocsV2 = require("../../info_api/documentacao/swagger/v2/swaggerV2.config.json");
->>>>>>> c1b15da34c6995aa35f96da988dfedbe7728dec0
 
 module.exports = function (app) {
   //Swagger Routes
