dc857d75fb72e828f0e2692cba1776aeea469eeb
Lucca <mario-lucca@hotmail.com>
2021-11-03 08:34:16 -0300

    Delete src directory

--- src/helpers/index.js
+++
@@ -1,9 +0,0 @@
-const mailer = require("./mailer");
-const inserirRegistros = require("./inserirRegistros")
-const verifyConstraints = require("./verifyConstraints")
-
-module.exports = {
-    mailer,
-    inserirRegistros,
-    verifyConstraints
-};
